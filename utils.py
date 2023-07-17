45c82a21d76533b9f9f1156841166fc4df85cb3b
Shashank <shashank.srivastava25sks@gmail.com>
2023-07-18 02:20:43 +0530

    Updated Data

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 import os
 import requests
 import torch
+from paddleocr import PaddleOCR
 
 def get_objects_in_image(file_name):
     torch.cuda.empty_cache()
@@ -22,17 +23,37 @@ def get_objects_in_image(file_name):
     return result
 
 def get_text_from_images(file_name):
-    reader = easyocr.Reader(['en','hi'], gpu = False)
-    textValues = reader.readtext(file_name)
-    result = []
-    
-    for text in textValues:
-        result.append({
-            'text': text[1],
-            'probability': text[2]
-        })
+        reader = easyocr.Reader(['hi'], gpu = False)
+        textValues = reader.readtext(file_name)
         
-    return result
+        hiResults = []
+        for text in textValues:
+            if text[2] >= 0.2:
+                hiResults.append({
+                        'text': text[1],
+                        'probability': text[2]
+                })
+        
+        
+        engResults = []
+        ocr = PaddleOCR(use_angle_cls=True, lang='en')
+        img_path = file_name
+        result = ocr.ocr(img_path, cls=True)
+        for idx in range(len(result)):
+                res = result[idx]
+                for line in res:
+                    if line[1][1] >= 0.5:
+                        engResults.append({
+                            'text': line[1][0],
+                            'probability': line[1][1]
+                        })
+        
+        results = hiResults + engResults
+        # result = engResults if engAvg > hiAvg else hiResults
+        # print(results)
+        for result in results:
+            print(result)
+        return results
 
 def download_data_from_FTP(url):
     a = urlparse(url)
